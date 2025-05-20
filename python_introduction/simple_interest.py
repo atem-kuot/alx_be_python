47528946633b6d28f4fbad0ef58e610e20044c2b
Atem Kuot <atemliaikuot@gmail.com>
2025-05-20 19:23:24 +0300

    Add simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+principal = 1000  # Principal amount
+rate = 0.05  # Interest rate in percentage
+time = 3  # Time in years
+
+# Calculate simple interest
+interest = (principal * rate * time)
+
+print("The simple interest is:", interest)
