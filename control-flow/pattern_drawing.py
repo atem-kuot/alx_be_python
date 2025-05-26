846d1c20f4455e92dc34444c61165e8b39d8fd92
Atem Kuot <atemliaikuot@gmail.com>
2025-05-26 19:16:23 +0300

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,8 +1,9 @@
+
 rows = int(input("Enter the size of the pattern: "))
 
-for i in range(rows):
-    # Print stars
+i = 0  
+while i < rows:
     for star in range(rows):
-        print("*", end="")
-
-    print()  # Move to the next line
+        print("*", end="")  
+    print() 
+    i += 1  # Move to the next row
