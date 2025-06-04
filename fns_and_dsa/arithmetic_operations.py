2f457bd3912adf4756563b56f6883ff452ca8283
Atem Kuot <atemliaikuot@gmail.com>
2025-06-04 10:45:25 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,13 @@
 def perform_operation(num1, num2, operation):
-    if operation == '+':
+    if operation == 'add':
         return num1 + num2
-    elif operation == '-':
+    elif operation == 'subtract':
         return num1 - num2
-    elif operation == '*':
+    elif operation == 'multiply':
         return num1 * num2
-    elif operation == '/':
+    elif operation == 'divide':
         if num2 == 0:
-            return "Can't be Divided by zero"
+            return "Can't be divided by zero"
         return num1 / num2
     else:
         return "Invalid operation"
