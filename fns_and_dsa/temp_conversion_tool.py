a24c879bfad64a629419fc1bff6def8e4bf60151
Atem Kuot <atemliaikuot@gmail.com>
2025-06-08 14:53:58 +0300

    Add temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,11 +1,11 @@
 # Global conversion factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
+    global FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
+    global CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 
