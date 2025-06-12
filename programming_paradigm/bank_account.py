557fccd03fe63a1161de9a85c196bbcb0d19b019
Atem Kuot <atemliaikuot@gmail.com>
2025-06-12 16:17:22 +0300

    Add bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,8 +1,8 @@
-class BankAccount ():
+class BankAccount():
 
     def __init__(self,account_balance ):
         self.account_balance = account_balance 
-        account_balance = 0
+    
     def deposit (self, amount):
         if amount > 0:
             self.account_balance += amount
