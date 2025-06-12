a5a86b34d42df197f38f04575e1119c79782a50f
Atem Kuot <atemliaikuot@gmail.com>
2025-06-12 20:40:25 +0300

    Add bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -18,4 +18,4 @@ class BankAccount:
                  
     def display_balance(self):
         self.account_balance = float(self.account_balance)
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
