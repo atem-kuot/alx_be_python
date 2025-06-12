b9c5bdcaffc82b5b93ada91b8936348b76347ac6
Atem Kuot <atemliaikuot@gmail.com>
2025-06-12 16:20:27 +0300

    Add bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,18 +6,18 @@ class BankAccount:
     def deposit (self, amount):
         if amount > 0:
             self.account_balance += amount
-            print(f"Deposited: ${amount}. New balance: ${self.account_balance}.")
+            print(f"Deposited: ${amount}. New balance: ${self.account_balance}")
         else:
-            print("Deposit amount must be positive.")
+            print("Deposit amount must be positive")
 
     def withdraw(self, amount):
         if amount > 0 and amount <= self.account_balance:
             self.account_balance -= amount
-            print(f"Withdrew: ${amount}. New balance: ${self.account_balance}.")
+            print(f"Withdrew: ${amount}. New balance: ${self.account_balance}")
         elif amount > self.account_balance:
-            print("Insufficient funds for this withdrawal.")
+            print("Insufficient funds for this withdrawal")
         else:
-            print("Withdrawal amount must be positive.")
+            print("Withdrawal amount must be positive")
     
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance}.")
+        print(f"Current balance: ${self.account_balance}")
